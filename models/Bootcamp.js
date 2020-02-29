13ee7b661af8b7fb591c2ced9cec99f3c17b1389
mypinkert <mypinkert@gmail.com>
2020-02-29 15:03:53 +0100

    initial commit2

---
+++ models/Bootcamp.js
@@ -0,0 +1,148 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
+
+const BootcampSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Name can not be more than 50 characters']
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description can not be more than 500 characters']
+    },
+    website: {
+        type: String,
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+            'Please use a valid URL with HTTP or HTTPS'
+        ]
+    },
+    phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters']
+    },
+    email: {
+        type: String,
+        match: [
+            /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/,
+            'Please add a valid email'
+            ]   
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    location: {
+    // GeoJSONpoint
+        type: {
+            type: [Number], 
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            index: '2dsphere'            
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String
+    },
+    careers: {
+        // Array of Strings
+        type: [String],
+        required: true,
+        enum: [
+            'Web Development',
+            'Mobile Development',
+            'UI/UX',
+            'Data Science',
+            'Business',
+            'Other'
+        ]
+    },
+    averageRating: {
+        type: Number,
+        min: [1, 'rating must be at leats 1'],
+        max: [10, 'rating can not be more than 1']
+    },
+    averageCost: Number,
+    photo: {
+        type: String,
+        default: 'no-photo.jpg'
+    },
+    housing: {
+        type: Boolean,
+        default: false
+    },
+    jobAssistance: {
+        type: Boolean,
+        default: false
+    },
+    jobGuarantee: {
+        type: Boolean,
+        default: false
+    },
+    acceptGI: {
+        type: Boolean,
+        default: false
+    },
+    cratedAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true}
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true })
+    next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipCode,
+        country: loc[0].countryCode,
+    }
+
+    // Do not save Address in DB
+    this.address = undefined;
+    next();
+});
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+    console.log(`Courses being removed from bootcamp ${this._id}`);
+    
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    next();
+});
+
+// Reverse populate with virtuals
+BootcampSchema.virtual('courses', {
+    ref: 'Course',
+    localField: '_id',
+    foreignField: 'bootcamp',
+    justOne: false
+})
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
--- server.js
+++ server.js
@@ -1,11 +1,55 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const fileupload = require('express-fileupload');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+
+// const logger = require('./middleware/logger');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// File uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// handle unhandled promise rejections
+const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Cose server & exit process
+    server.close(() => process.exit(1));
+});
